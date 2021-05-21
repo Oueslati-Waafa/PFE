837939962cb1a1df37a3a19d39291608044a82e6
Oueslati-Waafa <oueslatiwafa454@gmail.com>
2021-05-21 01:30:26 +0100

    first commit

---
+++ tekapp/database/migrations/2021_05_20_114955_create_contracts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateContractsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contracts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('professor_id');
+            $table->date('datedebut');
+            $table->date('datefin');
+            $table->string('type');
+            $table->timestamps();
+            $table->foreign('professor_id')->references('id')->on('professors')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contracts');
+    }
+}
